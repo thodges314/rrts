f493a8834bf75433f16cd5b4d7c3e303d887e7c4
Thomas Hodges <thodges@javascriptonline.org>
2019-10-22 16:44:28 -0700

    functional component

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,27 @@ import ReactDOM from 'react-dom';
 interface AppProps {
   color?: string;
 }
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
 
-  onIncrement = (): void => this.setState({ counter: this.state.counter + 1 });
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
 
-  onDecrement = (): void => this.setState({ counter: this.state.counter - 1 });
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>increment</button>
-        <button onClick={this.onDecrement}>decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   onIncrement = (): void => this.setState({ counter: this.state.counter + 1 });
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+//   onDecrement = (): void => this.setState({ counter: this.state.counter - 1 });
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>increment</button>
+//         <button onClick={this.onDecrement}>decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
